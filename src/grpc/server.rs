0fc64fbadb970399bee65209cec80d1b22c553c5
tstaetter <thomas.staetter@gmail.com>
2024-03-13 07:10:47 +0100

    feat: fixed service creation

--- src/grpc/server.rs
+++ src/grpc/server.rs
@@ -3,12 +3,14 @@ use tonic::transport::{Server};
 use crate::error::ShaResult;
 use crate::grpc::authentication_service::AuthenticationServer;
 use crate::grpc::file_service::FileServer;
+use crate::protobuf::file_service_server::FileServiceServer;
+use crate::protobuf::authentication_service_server::AuthenticationServiceServer;
 
 /// Register all services to a gRPC server and return it to
 /// be used in the main fn
 pub fn grpc_server() -> ShaResult<Router> {
-    let auth_svc = AuthenticationServer::default();
-    let file_svc = FileServer::default().into();
+    let auth_svc = AuthenticationServiceServer::new(AuthenticationServer::default());
+    let file_svc = FileServiceServer::new(FileServer::default());
 
     Ok(Server::builder()
         .accept_http1(true)
